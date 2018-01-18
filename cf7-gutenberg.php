663e98ec75589b4875406e51dd7b68bfd0c5a0f4
selu91 <cristea.marius.alexandru@gmail.com>
2018-01-18 17:14:26 +0200

    Fix plugin metadata.

--- cf7-gutenberg.php
+++ cf7-gutenberg.php
@@ -1,14 +1,14 @@
 <?php
 /**
- * Plugin Name:       CF 7 Gutenberg
- * Plugin URI:        https://themeisle.com/plugins/orbit-fox-companion
+ * Plugin Name:       Contact Form 7 - Gutenberg block
+ * Plugin URI:        https://github.com/Codeinwp/cf7-gutenberg
  * Description:       Registers and handles the Gutenberg block for Contact Form 7 plugin
  * Version:           1.0.0
  * Author:            Andrei Lupu
  * Author URI:        https://themeisle.com
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- * Text Domain:       themeisle
+ * Text Domain:       cf-7-gutenberg
  * Domain Path:       /languages
  * WordPress Available:  yes
  * Requires License:    no
@@ -21,12 +21,12 @@ if ( ! defined( 'WPINC' ) ) {
 
 add_action(
 	'plugins_loaded', function () {
-		// call this only if Gutenberg is active
-		if ( function_exists( 'register_block_type' ) ) {
-			// @TODO maybe wrap this into a function
-			require_once dirname( __FILE__ ) . '/class-cf7-gutenberg-block-plugin.php';
-			CF7_GutenbergBlockPlugin::instance();
-		}
+	// call this only if Gutenberg is active
+	if ( function_exists( 'register_block_type' ) ) {
+		// @TODO maybe wrap this into a function
+		require_once dirname( __FILE__ ) . '/class-cf7-gutenberg-block-plugin.php';
+		CF7_GutenbergBlockPlugin::instance();
 	}
+}
 );
 
